cf4b350058723f65106f4ad8ab648e8d68cfa5fb
Jaroslaw Piskorski <jaropis@zg.home.pl>
2025-03-28 14:27:07 +0100

    the first version works - the graph is visible

--- src/components/Tachogram.jsx
+++ src/components/Tachogram.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Dygraph from "dygraphs";
+
 const logRange = (minDate, maxDate, label) => {
   let start = new Date(minDate).toISOString().split("T")[0];
   let end = new Date(maxDate).toISOString().split("T")[0];
@@ -14,6 +15,7 @@ const parseDiff = (data, selectedColumn) => {
       parseFloat(data[i][selectedColumn]);
     localPlottingData.push([i, value]);
   }
+  console.log("localPlottingData", localPlottingData);
   return localPlottingData;
 };
 
@@ -28,6 +30,7 @@ const parseNoDiff = (data, selectedColumn) => {
 const Tachogram = ({ selectedColumn, data }) => {
   const diff = true;
   const [plottingData, setPlottingData] = useState(null);
+  const tachoGraph = useRef();
   useEffect(() => {
     if (data && selectedColumn >= 0) {
       if (diff) {
@@ -37,28 +40,51 @@ const Tachogram = ({ selectedColumn, data }) => {
       }
     }
   }, [data, selectedColumn, diff]);
-  console.log("plotting Data", plottingData);
-  // new Dygraph(document.getElementById("graphdiv"), data, {
-  //   title: "Time Series with Final Range Logging",
-  //   ylabel: "Value",
-  //   legend: "always",
-  //   showRangeSelector: true,
-  //   // Set an initial date window different from the data extremes
-  //   // so that slider interaction is more evident.
-  //   dateWindow: [
-  //     new Date("2020-01-02").getTime(),
-  //     new Date("2020-01-08").getTime(),
-  //   ],
-  //   rangeSelectorCallback: function (minDate, maxDate, yRanges) {
-  //     // callback fired during slider interaction.
-  //     logRange(minDate, maxDate, "Range Selector Callback");
-  //   },
-  //   zoomCallback: function (minDate, maxDate, yRanges) {
-  //     // this callback is fired when the main view (zoom) is updated,
-  //     // which happens after the user has finished moving the slider.
-  //     logRange(minDate, maxDate, "Zoom Callback");
-  //   },
-  // });
+
+  useEffect(() => {
+    if (plottingData && plottingData.length > 0) {
+      console.log("preparing dygraph");
+      tachoGraph.current = new Dygraph(
+        document.getElementById("graphdiv"),
+        plottingData,
+        {
+          title: "",
+          legend: "always",
+          labels: ["N", "RR"],
+          xlabel: "Beat Index",
+          ylabel: "RR",
+          x: {
+            axisLabelFormatter: function (x) {
+              return x.toFixed(0);
+            },
+            valueFormatter: function (x) {
+              return x.toFixed(0);
+            },
+          },
+          showRangeSelector: true,
+          // Set an initial date window different from the data extremes
+          // so that slider interaction is more evident.
+          rangeSelectorCallback: function (minDate, maxDate, yRanges) {
+            // callback fired during slider interaction.
+            logRange(minDate, maxDate, "Range Selector Callback");
+            console.log("2");
+          },
+          zoomCallback: function (minDate, maxDate, yRanges) {
+            // this callback is fired when the main view (zoom) is updated,
+            // which happens after the user has finished moving the slider.
+            logRange(minDate, maxDate, "Zoom Callback");
+            console.log("1");
+          },
+        },
+      );
+    }
+    return () => {
+      if (tachoGraph.current) {
+        tachoGraph.current.destroy();
+        tachoGraph.current = null;
+      }
+    };
+  }, [plottingData]);
   return <div id="graphdiv" style={{ width: "100%", height: "300px" }}></div>;
 };
 
